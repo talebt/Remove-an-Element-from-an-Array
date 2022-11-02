0b05d9b56b4275c42ebaf188703c11e38b06e5cf
Thabet Taleb <46090389+talebt@users.noreply.github.com>
2022-11-01 19:36:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Remove an Element from an Array
+
+
+let dailyActivities = ['work', 'eat', 'sleep', 'exercise'];
+
+// remove the last element
+dailyActivities.pop();
+console.log(dailyActivities); // ['work', 'eat', 'sleep']
+
+// remove the last element from ['work', 'eat', 'sleep']
+const removedElement = dailyActivities.pop();
+
+//get removed element
+console.log(removedElement); // 'sleep'
+console.log(dailyActivities);  // ['work', 'eat']
